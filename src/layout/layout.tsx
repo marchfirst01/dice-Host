44b4eb2aeb0de135faca8f3032289852f5791295
marchfirst01 <marchfirst3113@gmail.com>
2025-01-16 01:56:24 +0900

    Feat: bottomNavigation 페이지 연결

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,14 +1,20 @@
 import BottomNavigationComponent from '@components/bottomNavigation.tsx/bottomNavigation';
 import TopNavigation from '@components/topNavigation/topNavigation';
 
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default function MainLayout({
+  children,
+  setPageType,
+}: {
+  children: React.ReactNode;
+  setPageType: Dispatch<SetStateAction<'popUp' | 'recruit' | 'my'>>;
+}) {
   return (
     <div className="">
       <TopNavigation />
       <div className="px-5">{children}</div>
-      <BottomNavigationComponent />
+      <BottomNavigationComponent setPageType={setPageType} />
     </div>
   );
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -5,6 +5,13 @@ import React, { useState } from 'react';
 import PopUpPage from '../popUp';
 
 export default function MainPage() {
-  const [pageType, setPageType] = useState<'popUp' | 'recruit'>('popUp');
-  return <MainLayout>{pageType === 'popUp' && <PopUpPage />}</MainLayout>;
+  const [pageType, setPageType] = useState<'popUp' | 'recruit' | 'my'>('popUp');
+
+  return (
+    <MainLayout setPageType={setPageType}>
+      {pageType === 'popUp' && <PopUpPage />}
+      {pageType === 'recruit' && <p>recruit</p>}
+      {pageType === 'my' && <p>my</p>}
+    </MainLayout>
+  );
 }
