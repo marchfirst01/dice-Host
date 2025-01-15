41433bc2b99381ba77e65be0bd90a8753858fb6f
marchfirst01 <marchfirst3113@gmail.com>
2025-01-16 01:15:58 +0900

    Design: global css 수정, MainLayout 적용

---
+++ src/layout/layout.tsx
@@ -0,0 +1,17 @@
+import BottomNavigationComponent from "@components/bottomNavigation.tsx/bottomNavigation";
+import TopNavigation from "@components/topNavigation/topNavigation";
+import React from "react";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="">
+      <TopNavigation />
+      <div className="px-5">{children}</div>
+      <BottomNavigationComponent />
+    </div>
+  );
+}
