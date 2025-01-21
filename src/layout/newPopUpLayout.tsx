f1a40aad3e0e3e29a32f9e42710dcb326db5a57e
marchfirst01 <marchfirst3113@gmail.com>
2025-01-22 00:42:39 +0900

    Feat: 공간 등록 페이지 스크롤 시 헤더 변경

--- src/layout/newPopUpLayout.tsx
+++ src/layout/newPopUpLayout.tsx
@@ -1,7 +1,7 @@
 import ArrowBack from '@assets/topNavigation/arrow-back.svg';
 import { NewPopUpFormData } from '@type/newPopUp/newPopUpTypes';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SubmitHandler } from 'react-hook-form';
 
 import Image from 'next/image';
@@ -19,19 +19,51 @@ const NewPopUpLayout = ({ children, handleSubmit, onSubmit }: NewPopUpLayoutProp
   const router = useRouter();
   const childrenArray = React.Children.toArray(children);
 
+  const [isScrolled, setIsScrolled] = useState(false);
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > 110) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
-      <nav className="fixed z-50 h-12 w-full max-w-[400px] bg-black">
-        <Image
-          onClick={() => router.back()}
-          className="m-3 cursor-pointer"
-          src={ArrowBack}
-          alt="arrow-back"
-          width={24}
-          height={24}
-        />
-      </nav>
-      <div className="pb-[119px] pt-[72px]">
+    <div className="relative">
+      <header className="fixed z-50 flex h-[48px] w-full max-w-[400px] flex-row items-center bg-black">
+        <div className="relative flex h-[48px] w-full items-center justify-center">
+          <Image
+            onClick={() => router.back()}
+            className="absolute left-0 top-0 m-3 cursor-pointer"
+            src={ArrowBack}
+            alt="arrow-back"
+            width={24}
+            height={24}
+          />
+          {isScrolled && (
+            <p className="mx-auto font-BODY1 text-BODY1 leading-BODY1 text-white">
+              팝업 공간 등록 관리자 페이지
+            </p>
+          )}
+        </div>
+      </header>
+      <div className="mb-5 bg-black pt-[48px]">
+        <p className="px-5 pb-6 pt-8 font-H1 text-H1 leading-H1 text-white">
+          팝업 공간 등록
+          <br />
+          관리자 페이지
+        </p>
+      </div>
+      <div className="pb-[119px]">
         {childrenArray.map((child, index) => (
           <div className="flex flex-col" key={index}>
             <div className="px-5">{child}</div>
@@ -39,14 +71,14 @@ const NewPopUpLayout = ({ children, handleSubmit, onSubmit }: NewPopUpLayoutProp
           </div>
         ))}
       </div>
-      <nav className="fixed bottom-0 z-10 flex h-[84px] w-full max-w-[400px] flex-row items-center gap-3 border-t border-stroke bg-white px-5 py-4">
+      <footer className="fixed bottom-0 z-10 flex h-[84px] w-full max-w-[400px] flex-row items-center gap-3 border-t border-stroke bg-white px-5 py-4">
         <button
           onClick={handleSubmit(onSubmit)}
           className="flex h-full w-full cursor-pointer flex-row items-center justify-center gap-2 rounded-lg bg-black text-white"
         >
           <p>작성 완료</p>
         </button>
-      </nav>
+      </footer>
     </div>
   );
 };
