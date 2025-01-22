97ee525012fa77d13da857a38ea46dd7911a7b3f
marchfirst01 <marchfirst3113@gmail.com>
2025-01-22 13:16:58 +0900

    Chore: assets 폴더 구조 변경

---
+++ src/assets/index.ts
@@ -0,0 +1,69 @@
+// common/arrow
+import ArrowBackBlack from '@assets/common/arrow/arrow-back-black.svg';
+import ArrowBackWhite from '@assets/common/arrow/arrow-back-white.svg';
+import ArrowDownGray from '@assets/common/arrow/arrow-down-gray.svg';
+// common/logo
+import DiceBlack from '@assets/common/logo/dice-black.svg';
+import DiceWhite from '@assets/common/logo/dice-white.svg';
+import LogoBlack from '@assets/common/logo/logo-black.svg';
+import LogoWhite from '@assets/common/logo/logo-white.svg';
+// mainFooter
+import My from '@assets/mainFooter/my.svg';
+import Place from '@assets/mainFooter/place.svg';
+import Recruit from '@assets/mainFooter/recruit.svg';
+// mainHeader
+import Chat from '@assets/mainHeader/chat.svg';
+import Heart from '@assets/mainHeader/heart.svg';
+// member
+import Close from '@assets/member/close.svg';
+import Delete from '@assets/member/delete.svg';
+import EyeOff from '@assets/member/eye-off.svg';
+import EyeOn from '@assets/member/eye-on.svg';
+// popUp
+import HeaderBack from '@assets/popUp/headerBack.svg';
+import Search from '@assets/popUp/search.svg';
+// popUpDetail
+import Globe from '@assets/popUpDetail/globe.svg';
+import Phone from '@assets/popUpDetail/phone.svg';
+import PlaceMarker from '@assets/popUpDetail/place-marker.svg';
+import Send from '@assets/popUpDetail/send.svg';
+// popUpSetting
+import ImageDelete from '@assets/popUpSetting/image-delete.svg';
+import ImageUpload from '@assets/popUpSetting/upload.svg';
+
+const IMAGES = {
+  // common/arrow
+  ArrowBackBlack,
+  ArrowBackWhite,
+  ArrowDownGray,
+  // common/logo
+  DiceBlack,
+  DiceWhite,
+  LogoBlack,
+  LogoWhite,
+  // mainFooter
+  My,
+  Place,
+  Recruit,
+  //mainHeader
+  Chat,
+  Heart,
+  // member
+  Close,
+  Delete,
+  EyeOff,
+  EyeOn,
+  // popUp
+  HeaderBack,
+  Search,
+  // popUpDetail
+  Globe,
+  Phone,
+  PlaceMarker,
+  Send,
+  // popUpSetting
+  ImageDelete,
+  ImageUpload,
+};
+
+export { IMAGES };
