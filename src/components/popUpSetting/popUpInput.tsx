89dde47ed3d0fe5d4b8db1ba05752454f8c9dd68
marchfirst01 <marchfirst3113@gmail.com>
2025-01-23 15:58:33 +0900

    Feat: 전화번호 입력 형식

--- src/components/popUpSetting/popUpInput.tsx
+++ src/components/popUpSetting/popUpInput.tsx
@@ -12,14 +12,32 @@ export default function PopUpInputComponent({
   popUpConfig,
   control,
 }: PopUpInputComponentProps): React.ReactElement<PopUpInputComponentProps> {
+  const handlePhoneNumberInputChange =
+    (onChange: Function) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      let value = e.target.value.replace(/[^0-9]/g, ''); // 숫자만 남기기
+
+      // 000-0000-0000 형식으로 변환
+      if (value.length <= 3) {
+        value = value;
+      } else if (value.length <= 7) {
+        value = value.slice(0, 3) + '-' + value.slice(3);
+      } else {
+        value = value.slice(0, 3) + '-' + value.slice(3, 7) + '-' + value.slice(7, 11);
+      }
+      onChange(value);
+    };
+
   return (
     <Controller
       name={popUpConfig.name}
       control={control}
       render={({ field: { onChange, value = '' } }) => (
         <input
+          id="input"
           className="h-[44px] w-full rounded-lg border p-4 font-CAP1 text-CAP1 leading-CAP1"
-          onChange={onChange}
+          onChange={
+            popUpConfig.name === 'phoneNumber' ? handlePhoneNumberInputChange(onChange) : onChange
+          }
           value={value}
           placeholder={popUpConfig.placeholder}
         />
