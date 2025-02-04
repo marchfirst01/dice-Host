f7e0f7a65fbbba6ae33680960693b54a93383965
marchfirst01 <marchfirst3113@gmail.com>
2025-02-04 14:47:44 +0900

    Feat: 공간 상세 조회 api (getServerSideProps)

--- src/api/popUp.ts
+++ src/api/popUp.ts
@@ -1,8 +1,17 @@
 import { GetAxiosInstance } from '@axios/axios.method';
-import { SpaceLatestResponse } from '@type/popUp/popUpResponse';
+import { GuestGetAxiosInstance } from '@axios/guest.axios.method';
+import { SpaceIdResponse, SpaceLatestResponse } from '@type/popUp/popUpResponse';
 
 export const fetchSpaceLatest = async (): Promise<SpaceLatestResponse> => {
   const res = await GetAxiosInstance(`/space/latest?page=0&size=10`);
+  if (res.status !== 200) throw new Error('Failed to fetch posts');
+  return res.data;
+};
+
+// getServerSideProps로 실행
+export const fetchSpaceId = async (id: string): Promise<SpaceIdResponse> => {
+  console.log('fetchSpaceId, ', id);
+  const res = await GuestGetAxiosInstance(`/space/${id}`);
   if (res.status !== 200) throw new Error('Failed to fetch posts');
   return res.data;
 };
--- src/type/popUp/popUpResponse.ts
+++ src/type/popUp/popUpResponse.ts
@@ -28,3 +28,28 @@ export interface Sort {
   sorted: boolean;
   unsorted: boolean;
 }
+
+export interface SpaceIdResponse {
+  id: number;
+  name: string;
+  description: string;
+  imageUrls: string[];
+  category: string;
+  openingTime: string;
+  closingTime: string;
+  capacity: number;
+  tags: string[];
+  pricePerDay: number;
+  discountRate: number;
+  details: string;
+  latitude: number;
+  longitude: number;
+  city: string;
+  district: string;
+  address: string;
+  websiteUrl: string;
+  contactNumber: string;
+  facilityInfo: string;
+  notice: string;
+  likeCount: number;
+}
