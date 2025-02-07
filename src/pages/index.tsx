a33689cd6bb8e83be1025708729bb1da143fa0a5
marchfirst01 <marchfirst3113@gmail.com>
2025-02-07 23:23:18 +0900

    Design: index 페이지 디자인 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <p className="text-center font-SUB2 text-SUB2">팝업 운영 올인원 솔루션</p>
       </div>
 
-      <div className="fixed bottom-[34px] space-y-[11px] pb-5 pt-4">
+      <div className="fixed bottom-[34px] w-full max-w-[400px] space-y-[11px] px-5 pb-5 pt-4">
         <button
           onClick={() => router.push(`/member/login`)}
           className="flex w-full flex-row items-center justify-center space-x-2 rounded-lg bg-black p-4"
@@ -23,18 +23,12 @@ export default function Home() {
         </button>
 
         <div className="flex flex-row items-center justify-center">
+          <p className="py-2.5 font-BTN1 text-BTN1 text-medium_gray">아직 회원이 아니신가요?</p>
           <button
             onClick={() => router.push(`/member/register`)}
             className="px-4 py-2.5 font-BTN1 text-BTN1 text-medium_gray underline"
           >
             회원으로 가입하기
-          </button>
-          <p className="font-BTN1 text-BTN1 text-medium_gray">|</p>
-          <button
-            onClick={() => router.push(`/main`)}
-            className="px-4 py-2.5 font-BTN1 text-BTN1 text-medium_gray underline"
-          >
-            비회원으로 둘러보기
           </button>
         </div>
       </div>
