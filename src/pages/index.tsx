680794ec88f6ecf78ecc2cfaa25453b7a5b4e9a2
marchfirst01 <marchfirst3113@gmail.com>
2025-02-08 14:24:06 +0900

    Feat: 로그인 한 뒤 로그인 페이지 접근 시 메인페이지로 다시 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import { IMAGES } from '@assets/index';
+import { getAccessToken } from '@utils/token';
+
+import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    setIsLoggedIn(getAccessToken() ? true : false);
+  }, []);
 
-  return (
+  return isLoggedIn ? (
+    router.push('/main')
+  ) : (
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="space-y-4">
         <Image src={IMAGES.LogoBlack} alt="로고 이미지" />
