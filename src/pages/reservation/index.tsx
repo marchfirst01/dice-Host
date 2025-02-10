e194e1044e791c9c17e6d38045cd7ce311aea5b8
marchfirst01 <marchfirst3113@gmail.com>
2025-02-10 11:03:25 +0900

    Feat: 예약 상태에 따른 필터링 적용

--- src/pages/reservation/index.tsx
+++ src/pages/reservation/index.tsx
@@ -1,4 +1,5 @@
 import ReservationItemComponent from '@components/reservation/reservationItem';
+import { ReservationStatus } from '@type/reservation';
 
 import React, { useState } from 'react';
 
@@ -6,34 +7,36 @@ import { reservationDummy } from './reservationDummy';
 
 export default function ReservationPage() {
   const [reservationItem] = useState(reservationDummy);
+  const [reservationStatus, setReservationStatus] = useState<ReservationStatus>('pending');
   return (
     <>
+      {/* TODO: 클릭 된 아이템 표시 필요 */}
       <div className="fixed z-50 flex w-full max-w-[400px] -translate-x-5 flex-row justify-center bg-back_gray">
-        <button className="w-[111.67px] py-3 font-BTN1 text-BTN1 leading-BTN1">대기중</button>
-        <button className="w-[111.67px] py-3 font-BTN1 text-BTN1 leading-BTN1">예약 완료</button>
-        <button className="w-[111.67px] py-3 font-BTN1 text-BTN1 leading-BTN1">예약 취소</button>
+        <button
+          onClick={() => setReservationStatus('pending')}
+          className="w-[111.67px] py-3 font-BTN1 text-BTN1 leading-BTN1"
+        >
+          대기중
+        </button>
+        <button
+          onClick={() => setReservationStatus('confirmed')}
+          className="w-[111.67px] py-3 font-BTN1 text-BTN1 leading-BTN1"
+        >
+          예약 완료
+        </button>
+        <button
+          onClick={() => setReservationStatus('cancled')}
+          className="w-[111.67px] py-3 font-BTN1 text-BTN1 leading-BTN1"
+        >
+          예약 취소
+        </button>
       </div>
       <div className="pt-[45px]">
-        <ReservationItemComponent
-          reservationItem={reservationItem[0]}
-          status={reservationItem[0].status}
-        />
-        <ReservationItemComponent
-          reservationItem={reservationItem[0]}
-          status={reservationItem[0].status}
-        />
-        <ReservationItemComponent
-          reservationItem={reservationItem[0]}
-          status={reservationItem[0].status}
-        />
-        <ReservationItemComponent
-          reservationItem={reservationItem[0]}
-          status={reservationItem[0].status}
-        />
-        <ReservationItemComponent
-          reservationItem={reservationItem[0]}
-          status={reservationItem[0].status}
-        />
+        {reservationItem
+          .filter((status) => status.status === reservationStatus)
+          .map((item, index) => (
+            <ReservationItemComponent key={index} reservationItem={item} status={item.status} />
+          ))}
       </div>
     </>
   );
