14d5d022bc4cbacf218c8cb10eb64d3235db17c8
marchfirst01 <marchfirst3113@gmail.com>
2025-02-19 16:09:54 +0900

    Feat: react-hook-form 연결

--- src/pages/my/update/index.tsx
+++ src/pages/my/update/index.tsx
@@ -1,31 +1,62 @@
 import { IMAGES } from '@assets/index';
+import { useHostInfo } from '@hooks/useHost';
+import { HostInfo } from '@type/my';
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function MyUpdatePage() {
   const router = useRouter();
+  const { register, handleSubmit, setValue } = useForm<HostInfo>();
+  const { data: defaultHostInfo } = useHostInfo();
+  const [bankName, setBankName] = useState(
+    defaultHostInfo.bankName ? defaultHostInfo.bankName : '',
+  );
+
+  const onSubmit = (hostInfo: HostInfo) => {
+    console.log(hostInfo);
+  };
+
+  const handleBankSelection = () => {
+    setBankName('신한은행');
+    setValue('bankName', '신한은행'); // 예제: 버튼 클릭 시 '신한은행'으로 값 설정
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <header className="flex flex-row bg-black">
         <div onClick={() => router.back()} className="cursor-pointer px-3 py-3">
           <Image src={IMAGES.ArrowBackWhite} alt="back" />
         </div>
         <p className="flex-grow py-3 text-center text-white">호스트 정보 수정</p>
-        <button className="px-5 text-white">완료</button>
+        <button type="submit" className="px-5 text-white">
+          완료
+        </button>
       </header>
       <div className="flex flex-col gap-6 px-5 py-8 font-CAP1 text-CAP1 leading-CAP1 text-dark_gray">
         <div>
           <p>호스트 이름</p>
-          <input className="mt-2 h-11 w-full rounded-lg border border-light_gray px-4" />
+          <input
+            {...register('name')}
+            defaultValue={defaultHostInfo.name}
+            className="mt-2 h-11 w-full rounded-lg border border-light_gray px-4"
+          />
         </div>
         <div>
           <p>이메일</p>
           <div className="mt-2 flex flex-row items-center justify-center gap-2">
-            <input className="h-11 w-full rounded-lg border border-light_gray px-4" />
-            <button className="h-11 text-nowrap rounded-lg border border-light_gray px-[41.5px] text-center text-light_gray">
+            <input
+              {...register('email')}
+              defaultValue={defaultHostInfo.email}
+              className="h-11 w-full rounded-lg border border-light_gray px-4"
+            />
+            <button
+              type="button"
+              className="h-11 text-nowrap rounded-lg border border-light_gray px-[41.5px] text-center text-light_gray"
+            >
               중복 확인
             </button>
           </div>
@@ -33,8 +64,15 @@ export default function MyUpdatePage() {
         <div>
           <p>휴대폰</p>
           <div className="mt-2 flex flex-row items-center justify-center gap-2">
-            <input className="h-11 w-full rounded-lg border border-light_gray px-4" />
-            <button className="h-11 text-nowrap rounded-lg border border-light_gray px-[41.5px] text-center text-light_gray">
+            <input
+              {...register('phone')}
+              defaultValue={defaultHostInfo.phone}
+              className="h-11 w-full rounded-lg border border-light_gray px-4"
+            />
+            <button
+              type="button"
+              className="h-11 text-nowrap rounded-lg border border-light_gray px-[41.5px] text-center text-light_gray"
+            >
               중복 확인
             </button>
           </div>
@@ -42,17 +80,28 @@ export default function MyUpdatePage() {
         <div>
           <p>계좌번호</p>
           <div className="relative flex flex-row">
-            <input className="mt-2 h-11 w-full rounded-lg border border-light_gray px-4" />
+            <input
+              {...register('accountNumber')}
+              defaultValue={defaultHostInfo.accountNumber ? defaultHostInfo.accountNumber : ''}
+              className="mt-2 h-11 w-full rounded-lg border border-light_gray px-4"
+              placeholder="계좌번호를 입력해주세요"
+            />
             <div className="absolute right-0 mt-2 flex flex-row items-center text-light_gray">
               <p>|</p>
-              <button className="h-11 w-[118px]">은행 선택</button>
+              <button type="button" onClick={handleBankSelection} className="h-11 w-[118px]">
+                {bankName ? bankName : '은행 선택'}
+              </button>
+              <input {...register('bankName')} type="hidden" />
             </div>
           </div>
         </div>
-        <button className="mt-8 h-[52px] w-full rounded-lg border border-stroke text-medium_gray">
+        <button
+          type="button"
+          className="mt-4 h-[52px] w-full rounded-lg border border-stroke text-medium_gray"
+        >
           비밀번호 재설정
         </button>
       </div>
-    </div>
+    </form>
   );
 }
