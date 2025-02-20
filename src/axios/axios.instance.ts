05aeab8acb28fad6d982c809a1fcf647c66f538c
marchfirst01 <marchfirst3113@gmail.com>
2025-02-20 17:28:43 +0900

    Feat: 토큰 만료 시 재발급

--- src/axios/axios.instance.ts
+++ src/axios/axios.instance.ts
@@ -1,6 +1,14 @@
-import { deleteToken, getAccessToken } from '@utils/token';
+import {
+  deleteToken,
+  getAccessToken,
+  getRefreshToken,
+  setAccessToken,
+  setRefreshToken,
+} from '@utils/token';
 
+import { GuestPostAxiosInstance } from './guest.axios.method';
 import axios, { AxiosInstance } from 'axios';
+import Router from 'next/router';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -25,6 +33,22 @@ axiosInstance.interceptors.request.use(
   },
 );
 
+const refreshTokenRequest = async () => {
+  try {
+    const refreshToken = await getRefreshToken();
+    const { data } = await GuestPostAxiosInstance('/auth/reissue', {
+      refreshToken: refreshToken,
+    });
+    await setAccessToken(data.accessToken);
+    await setRefreshToken(data.refreshToken);
+    return data;
+  } catch (error) {
+    deleteToken();
+    alert('로그인을 다시 해주세요');
+    Router.push('/');
+  }
+};
+
 // 토큰 관련 에러 처리
 axiosInstance.interceptors.response.use(
   async (response) => {
@@ -32,13 +56,26 @@ axiosInstance.interceptors.response.use(
   },
 
   async (error) => {
+    const originalRequest = error.config;
+
+    // 403 에러 발생 시 토큰 재발급 처리
+    if (error.response?.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true; // 무한 루프 방지
+
+      try {
+        const { data } = await refreshTokenRequest();
+        originalRequest.headers['Authorization'] = `Bearer ${data.accessToken}`;
+        return axiosInstance(originalRequest); // 기존 요청 재시도
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
     // 토큰 만료나 잘못된 토큰일 때 로그아웃 처리
     if (error.response?.data?.code === 'AUTH_001') {
       console.log('잘못된 토큰');
       deleteToken();
     }
-
-    // TODO: 토큰 만료시 재발급 처리
 
     return Promise.reject(error);
   },
