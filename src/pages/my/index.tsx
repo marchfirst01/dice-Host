6b2daf0c7ef81e18d056a6073e2fd9e1dffa8ed8
marchfirst01 <marchfirst3113@gmail.com>
2025-02-26 18:38:48 +0900

    Fix: pages/my hostInfo의 name undefined 문제 해결

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -31,65 +31,67 @@ export default function MyPage({
   };
 
   return (
-    <MyLayout>
-      <div>
-        <div className="flex flex-row items-center gap-3">
-          <Image src={IMAGES.DiceBlack} alt="profile" width={54} height={54} />
-          <p className="font-SUB1 text-SUB1 leading-SUB1">{hostInfo.name}</p>
-        </div>
+    hostInfo && (
+      <MyLayout>
         <div>
-          <div className="mt-4 flex flex-row items-center justify-between">
-            <p className="font-SUB3 text-SUB3 leading-SUB3">
-              등록한 공간 <span className="text-purple">{hostSpaceData.length}개</span>
-            </p>
-            <p
-              onClick={() => setMainPageType('popUp')}
-              className="cursor-pointer font-CAP2 text-CAP2 leading-CAP2 text-medium_gray underline"
-            >
-              리스트 바로가기
-            </p>
+          <div className="flex flex-row items-center gap-3">
+            <Image src={IMAGES.DiceBlack} alt="profile" width={54} height={54} />
+            <p className="font-SUB1 text-SUB1 leading-SUB1">{hostInfo.name}</p>
           </div>
-          <MySpaceListComponent mySpaceList={hostSpaceData} />
+          <div>
+            <div className="mt-4 flex flex-row items-center justify-between">
+              <p className="font-SUB3 text-SUB3 leading-SUB3">
+                등록한 공간 <span className="text-purple">{hostSpaceData.length}개</span>
+              </p>
+              <p
+                onClick={() => setMainPageType('popUp')}
+                className="cursor-pointer font-CAP2 text-CAP2 leading-CAP2 text-medium_gray underline"
+              >
+                리스트 바로가기
+              </p>
+            </div>
+            <MySpaceListComponent mySpaceList={hostSpaceData} />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-6 font-SUB3 text-SUB3 leading-SUB3 text-deep_gray">
-        <p onClick={() => router.push('/')} className="cursor-pointer">
-          찜한 목록
-        </p>
-        <p onClick={() => router.push('/')} className="cursor-pointer">
-          쪽지함
-        </p>
-        <hr />
-        <p onClick={() => router.push('/')} className="cursor-pointer">
-          회원 정보 관리
-        </p>
+        <div className="flex flex-col gap-6 font-SUB3 text-SUB3 leading-SUB3 text-deep_gray">
+          <p onClick={() => router.push('/')} className="cursor-pointer">
+            찜한 목록
+          </p>
+          <p onClick={() => router.push('/')} className="cursor-pointer">
+            쪽지함
+          </p>
+          <hr />
+          <p onClick={() => router.push('/')} className="cursor-pointer">
+            회원 정보 관리
+          </p>
+          <p
+            onClick={() =>
+              window.open('https://juvenile-chess-b24.notion.site/18e7ece7ecb5800e99a0eedd7976c022')
+            }
+            className="cursor-pointer"
+          >
+            이용 약관
+          </p>
+          <p
+            onClick={() =>
+              window.open('https://juvenile-chess-b24.notion.site/18e7ece7ecb5806dab25c6fe7c424d7c')
+            }
+            className="cursor-pointer"
+          >
+            개인정보 처리방침
+          </p>
+          <hr />
+          <p onClick={() => handleLogout()} className="cursor-pointer">
+            로그아웃
+          </p>
+        </div>
         <p
-          onClick={() =>
-            window.open('https://juvenile-chess-b24.notion.site/18e7ece7ecb5800e99a0eedd7976c022')
-          }
-          className="cursor-pointer"
+          onClick={() => router.push({ pathname: '/my/withDraw', query: { name: hostInfo.name } })}
+          className="cursor-pointer font-SUB3 text-SUB3 leading-SUB3 text-deep_gray"
         >
-          이용 약관
+          탈퇴하기
         </p>
-        <p
-          onClick={() =>
-            window.open('https://juvenile-chess-b24.notion.site/18e7ece7ecb5806dab25c6fe7c424d7c')
-          }
-          className="cursor-pointer"
-        >
-          개인정보 처리방침
-        </p>
-        <hr />
-        <p onClick={() => handleLogout()} className="cursor-pointer">
-          로그아웃
-        </p>
-      </div>
-      <p
-        onClick={() => router.push({ pathname: '/my/withDraw', query: { name: hostInfo.name } })}
-        className="cursor-pointer font-SUB3 text-SUB3 leading-SUB3 text-deep_gray"
-      >
-        탈퇴하기
-      </p>
-    </MyLayout>
+      </MyLayout>
+    )
   );
 }
