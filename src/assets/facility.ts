738c7fb480e02c079e2c3ae6f955358f2bd7db18
marchfirst01 <marchfirst3113@gmail.com>
2025-07-17 21:06:52 +0900

    Feat: facility icon 추가

---
+++ src/assets/facility.ts
@@ -0,0 +1,103 @@
+// facility
+import cctvGrey from '@assets/facility/cctv-grey.svg';
+import cctv from '@assets/facility/cctv.svg';
+import chairGrey from '@assets/facility/chair-grey.svg';
+import chair from '@assets/facility/chair.svg';
+import checkGrey from '@assets/facility/check-grey.svg';
+import check from '@assets/facility/check.svg';
+import circleTableGrey from '@assets/facility/circle-table-grey.svg';
+import circleTable from '@assets/facility/circle-table.svg';
+import couchGrey from '@assets/facility/couch-grey.svg';
+import couch from '@assets/facility/couch.svg';
+import desktopGrey from '@assets/facility/desktop-grey.svg';
+import desktop from '@assets/facility/desktop.svg';
+import drinkGrey from '@assets/facility/drink-grey.svg';
+import drink from '@assets/facility/drink.svg';
+import fireExtinguisherGrey from '@assets/facility/fire-extinguisher-grey.svg';
+import fireExtinguisher from '@assets/facility/fire-extinguisher.svg';
+import firealarmGrey from '@assets/facility/firealarm-grey.svg';
+import firealarm from '@assets/facility/firealarm.svg';
+import firstAidKitGrey from '@assets/facility/first-aid-kit-grey.svg';
+import firstAidKit from '@assets/facility/first-aid-kit.svg';
+import lightGrey from '@assets/facility/light-grey.svg';
+import light from '@assets/facility/light.svg';
+import minusGrey from '@assets/facility/minus-grey.svg';
+import minus from '@assets/facility/minus.svg';
+import monitorGrey from '@assets/facility/monitor-grey.svg';
+import monitor from '@assets/facility/monitor.svg';
+import plusGrey from '@assets/facility/plus-grey.svg';
+import plus from '@assets/facility/plus.svg';
+import printerGrey from '@assets/facility/printer-grey.svg';
+import printer from '@assets/facility/printer.svg';
+import projectorGrey from '@assets/facility/projector-grey.svg';
+import projector from '@assets/facility/projector.svg';
+import shelfGrey from '@assets/facility/shelf-grey.svg';
+import shelf from '@assets/facility/shelf.svg';
+import speakerGrey from '@assets/facility/speaker-grey.svg';
+import speaker from '@assets/facility/speaker.svg';
+import squareTableGrey from '@assets/facility/square-table-grey.svg';
+import squareTable from '@assets/facility/square-table.svg';
+import standingTableGrey from '@assets/facility/standing-table-grey.svg';
+import standingTable from '@assets/facility/standing-table.svg';
+import tvGrey from '@assets/facility/tv-grey.svg';
+import tv from '@assets/facility/tv.svg';
+import waterPurifierGrey from '@assets/facility/water-purifier-grey.svg';
+import waterPurifier from '@assets/facility/water-purifier.svg';
+import wifiGrey from '@assets/facility/wifi-grey.svg';
+import wifi from '@assets/facility/wifi.svg';
+
+const FACILITY = {
+  // black
+  cctv,
+  chair,
+  circleTable,
+  couch,
+  desktop,
+  drink,
+  fireExtinguisher,
+  firealarm,
+  firstAidKit,
+  light,
+  monitor,
+  printer,
+  projector,
+  shelf,
+  speaker,
+  squareTable,
+  standingTable,
+  tv,
+  waterPurifier,
+  wifi,
+
+  // grey
+  cctvGrey,
+  chairGrey,
+  circleTableGrey,
+  couchGrey,
+  desktopGrey,
+  drinkGrey,
+  fireExtinguisherGrey,
+  firealarmGrey,
+  firstAidKitGrey,
+  lightGrey,
+  monitorGrey,
+  printerGrey,
+  projectorGrey,
+  shelfGrey,
+  speakerGrey,
+  squareTableGrey,
+  standingTableGrey,
+  tvGrey,
+  waterPurifierGrey,
+  wifiGrey,
+
+  // btn
+  plus,
+  plusGrey,
+  minus,
+  minusGrey,
+  check,
+  checkGrey,
+};
+
+export { FACILITY };
